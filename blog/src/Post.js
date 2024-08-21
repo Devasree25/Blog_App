ee495814102173f4bb80dfd2a1b4363dec913768
devasree A <devasree.a@kalvium.community>
2024-08-21 13:18:45 +0530

    building homepage with react

---
+++ blog/src/Post.js
@@ -0,0 +1,22 @@
+export default function Post(){
+    return(
+        <div className="post">
+        <div className="image">
+          <img src="https://media.istockphoto.com/id/1419410282/photo/silent-forest-in-spring-with-beautiful-bright-sun-rays.jpg?s=612x612&w=0&k=20&c=UHeb1pGOw6ozr6utsenXHhV19vW6oiPIxDqhKCS2Llk=" alt="" />
+
+        </div>
+
+
+        <div className="text">
+          <h2>Nature finds its way</h2>
+          <p className="info">
+            <a className="author">Devasree</a>
+            <time>2024-21-08 11.55</time>
+          </p>
+          <p className="summary">Nature is a teacher – What happens when you allow everything to be “as it is”?</p>
+
+        </div>
+
+      </div>
+    );
+}
